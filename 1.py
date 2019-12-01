27fc6a82d9de7cda28d3f17543977d36c08a2464
NIKALyb <58136351+NIKALyb@users.noreply.github.com>
2019-12-01 14:24:57 +0300

    Se

---
+++ 1.py
@@ -0,0 +1,9 @@
+#connect library
+import mcpi.minecraft as minecraft
+mc = minecraft.Minecraft.create()
+import mcpi.block as block
+import time
+import random
+#setBlock
+mc.setBlocks(51,130,51,-1,131,-1,22)
+mc.setBlocks(50,131,50,0,131,0,0)
